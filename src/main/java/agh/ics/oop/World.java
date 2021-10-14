df40e7163d0f3d255dd79d5aff6f5d4f131ff4e0
IIaMoCLitTMW <szulc.tobiasz@gmail.com>
2021-10-14 15:52:09 +0200

    instrukcja punkt 8

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,6 +4,11 @@ public class World {
     public static void main(String[] args)
     {
         System.out.println("system wystartował");
+        run();
         System.out.println("system zakończył działanie");
+    }
+    static void run()
+    {
+        System.out.println("zwierzak idzie do przodu");
     }
 }
