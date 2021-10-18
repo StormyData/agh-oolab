6108f5c0b18a73f5a375acdba03e53d015933071
IIaMoCLitTMW <szulc.tobiasz@gmail.com>
2021-10-18 18:04:46 +0200

    punkt 25 (dla zaawansowanych)

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,51 +1,36 @@
 package agh.ics.oop;
-
+import java.util.Collection;
+import java.util.stream.Stream;
 import java.util.Arrays;
 
 public class World {
     public static void main(String[] args)
     {
         System.out.println("system wystartował");
-        run(arg_converter(args));
+        run(arg_converter(Arrays.stream(args)));
         System.out.println("system zakończył działanie");
     }
 
-    private static Direction[] arg_converter(String[] args) {
-        Direction[] arr= new Direction[args.length];
-        int i=0;
-        for(String arg : args)
-        {
-            switch (arg)
-            {
-                case "f":
-                    arr[i++]=Direction.FORWARD;
-                    break;
-                case "b":
-                    arr[i++]=Direction.BACKWARD;
-                    break;
-                case "l":
-                    arr[i++]=Direction.LEFT;
-                    break;
-                case "r":
-                    arr[i++]=Direction.RIGHT;
-                    break;
-
-            }
-        }
-        return Arrays.copyOfRange(arr,0,i);
+    private static Stream<Direction> arg_converter(Stream<String> args) {
+        final Collection<String> symbols = Arrays.asList("f", "b", "l", "r");
+        return args.filter(x -> symbols.contains(x)).map(x->
+            switch (x) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                default -> Direction.FORWARD;
+            });
     }
 
-    private static void run(Direction[] args)
+    private static void run(Stream<Direction> args)
     {
-        for(Direction arg : args)
-        {
-            System.out.println(switch(arg)
+        args.map(x->switch(x)
             {
                 case FORWARD -> "zwierzak idzie do przodu";
                 case BACKWARD -> "zwierzak idzie do tyłu";
                 case RIGHT -> "zwierzak skręca w prawo";
                 case LEFT -> "zwierzak skręca w lewo";
-            });
-        }
+            }).forEach(x->System.out.println(x));
     }
 }
