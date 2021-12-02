b39ee48694753da69fa98cf1719d703224627c38
IIaMoCLitTMW <szulc.tobiasz@gmail.com>
2021-12-02 16:42:54 +0100

    zmiana MapBoundary na implementacje na posortowanym multisecie

--- src/main/java/agh/ics/oop/MapBoundary.java
+++ src/main/java/agh/ics/oop/MapBoundary.java
@@ -1,94 +1,73 @@
 package agh.ics.oop;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
-// ladniejsza implementacja z sorted multiset<Vector2d>
-// multiset np. na SotredMap<vector2d,int> gdzie int to ilosc wystapien
-public class MapBoundary {
+import java.util.*;
 
-    protected static class BoundaryXElement implements Comparable<BoundaryXElement>,IPositionChangeObserver
+public class MapBoundary implements IPositionChangeObserver{
+
+    static private final Comparator<Vector2d> comparatorX=new Comparator<Vector2d>() {
+        @Override
+        public int compare(Vector2d o1, Vector2d o2) {
+            if(o1.x!=o2.x)
+                return Integer.compare(o1.x,o2.x);
+            return Integer.compare(o1.y,o2.y);
+        }
+    };
+    static private final Comparator<Vector2d> comparatorY=new Comparator<Vector2d>() {
+        @Override
+        public int compare(Vector2d o1, Vector2d o2) {
+            if(o1.y!=o2.y)
+                return Integer.compare(o1.y,o2.y);
+            return Integer.compare(o1.x,o2.x);
+        }
+    };
+    private SortedMap<Vector2d,Integer> xPositions = new TreeMap<>(comparatorX);
+    private SortedMap<Vector2d,Integer> yPositions = new TreeMap<>(comparatorY);
+
+    private void addAtPos(Vector2d pos)
     {
-        private final int elementClassHashCode;
-        private Vector2d pos;
-        private final SortedSet<BoundaryXElement> treeSet;
-        BoundaryXElement(BaseWorldMapElement element, SortedSet<BoundaryXElement> treeSet)
-        {
-            pos=element.getPosition();
-            elementClassHashCode=element.getClass().hashCode();
-            this.treeSet = treeSet;
-            this.treeSet.add(this);
-            if(element instanceof IPositionChangedObservable observable)
-                observable.addObserver(this);
-        }
-        @Override
-        public int compareTo(BoundaryXElement o) {
-            if(pos.x!=o.pos.x)
-                return Integer.compare(pos.x,o.pos.x);
-            if(pos.y!=o.pos.y)
-                return Integer.compare(pos.y,o.pos.y);
-            return Integer.compare(elementClassHashCode,o.elementClassHashCode);
-        }
-        public Vector2d getPosition()
-        {
-            return pos;
-        }
+        if(xPositions.containsKey(pos))
+            xPositions.put(pos,xPositions.get(pos)+1);
+        else
+            xPositions.put(pos,1);
 
-        @Override
-        public void positionChanged(Vector2d oldPosition, Vector2d newPosition) {
-            treeSet.remove(this);
-            pos=newPosition;
-            treeSet.add(this);
-        }
+        if(yPositions.containsKey(pos))
+            yPositions.put(pos,yPositions.get(pos)+1);
+        else
+            yPositions.put(pos,1);
     }
-    protected static class BoundaryYElement implements Comparable<BoundaryYElement>,IPositionChangeObserver
+    private void removeFromPos(Vector2d pos)
     {
-        private final int elementClassHashCode;
-        private Vector2d pos;
-        private final SortedSet<BoundaryYElement> treeSet;
-        BoundaryYElement(BaseWorldMapElement element, SortedSet<BoundaryYElement> treeSet)
-        {
-            pos=element.getPosition();
-            elementClassHashCode=element.getClass().hashCode();
-            this.treeSet = treeSet;
-            this.treeSet.add(this);
-            if(element instanceof IPositionChangedObservable observable)
-                observable.addObserver(this);
-        }
-        @Override
-        public int compareTo(BoundaryYElement o) {
-            if(pos.y!=o.pos.y)
-                return Integer.compare(pos.y,o.pos.y);
-            if(pos.x!=o.pos.x)
-                return Integer.compare(pos.x,o.pos.x);
-            return Integer.compare(elementClassHashCode,o.elementClassHashCode);
-        }
-        public Vector2d getPosition()
-        {
-            return pos;
-        }
-        @Override
-        public void positionChanged(Vector2d oldPosition, Vector2d newPosition) {
-            treeSet.remove(this);
-            pos=newPosition;
-            treeSet.add(this);
-        }
+        if(!xPositions.containsKey(pos))
+            throw new IllegalArgumentException(String.format("unable to remove object from %s",pos));
+        if(!yPositions.containsKey(pos))
+            throw new IllegalArgumentException(String.format("unable to remove object from %s",pos));
+        if(xPositions.get(pos)==1)
+            xPositions.remove(pos);
+        else
+            xPositions.put(pos,xPositions.get(pos)-1);
+        if(yPositions.get(pos)==1)
+            yPositions.remove(pos);
+        else
+            yPositions.put(pos,yPositions.get(pos)-1);
     }
-
-    SortedSet<BoundaryXElement> xPositions = new TreeSet<>();
-    SortedSet<BoundaryYElement> yPositions = new TreeSet<>();
-
-
     public void addMapObject(BaseWorldMapElement el)
     {
-        new BoundaryXElement(el, xPositions);
-        new BoundaryYElement(el, yPositions);
+        if(el instanceof IPositionChangedObservable observable)
+            observable.addObserver(this);
+        addAtPos(el.getPosition());
     }
     public Vector2d getLowerLeft()
     {
-        return new Vector2d(xPositions.first().getPosition().x,yPositions.first().getPosition().y);
+        return new Vector2d(xPositions.firstKey().x,yPositions.firstKey().y);
     }
     public Vector2d getUpperRight()
     {
-        return new Vector2d(xPositions.last().getPosition().x,yPositions.last().getPosition().y);
+        return new Vector2d(xPositions.lastKey().x,yPositions.lastKey().y);
+    }
+
+    @Override
+    public void positionChanged(Vector2d oldPosition, Vector2d newPosition) {
+        removeFromPos(oldPosition);
+        addAtPos(newPosition);
     }
 }
