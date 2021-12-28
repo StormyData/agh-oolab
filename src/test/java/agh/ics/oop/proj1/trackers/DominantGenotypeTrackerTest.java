57d1e8d3d5eaf34e3f9dd9bad9b480d1f6ad322e
IIaMoCLitTMW <szulc.tobiasz@gmail.com>
2021-12-28 18:32:11 +0100

    changed DominantGenotypeTrackerTest to be deterministic and more exhaustive

--- src/test/java/agh/ics/oop/proj1/trackers/DominantGenotypeTrackerTest.java
+++ src/test/java/agh/ics/oop/proj1/trackers/DominantGenotypeTrackerTest.java
@@ -15,22 +15,29 @@ class DominantGenotypeTrackerTest {
         AbstractNonFlatWorldMap map = TestHelper.generateTestMap();
         DominantGenotypeTracker tracker = new DominantGenotypeTracker(map);
         assertTrue(tracker.getDominantGenotypes().isEmpty());
-        Genotype genotype1 = new Genotype();
+        Genotype genotype1 = new Genotype(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1});
         Genotype genotype2 = new Genotype(genotype1.getGenes());
         generateAnimalWithGenotype(map, genotype1);
         assertEquals(Set.of(genotype1), tracker.getDominantGenotypes());
         generateAnimalWithGenotype(map, genotype2);
         assertEquals(Set.of(genotype1), tracker.getDominantGenotypes());
-        Genotype genotype3 = new Genotype();
+        Genotype genotype3 = new Genotype(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3});
         generateAnimalWithGenotype(map, genotype3);
         generateAnimalWithGenotype(map, genotype3);
         assertEquals(Set.of(genotype1, genotype3), tracker.getDominantGenotypes());
-        generateAnimalWithGenotype(map, genotype3);
+        Animal animal3 = generateAnimalWithGenotype(map, genotype3);
         assertEquals(Set.of(genotype3), tracker.getDominantGenotypes());
+        Genotype genotype4 = new Genotype(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4});
+        Animal animal4 = generateAnimalWithGenotype(map,genotype4);
+        assertEquals(Set.of(genotype3), tracker.getDominantGenotypes());
+        map.remove(animal4);
+        assertEquals(Set.of(genotype3), tracker.getDominantGenotypes());
+        map.remove(animal3);
+        assertEquals(Set.of(genotype1, genotype3), tracker.getDominantGenotypes());
 
     }
 
-    private void generateAnimalWithGenotype(AbstractNonFlatWorldMap map, Genotype genotype) {
-        new Animal(map, new Vector2d(0, 0), MapDirection.NORTH, genotype, 5, 5, () -> 0);
+    private Animal generateAnimalWithGenotype(AbstractNonFlatWorldMap map, Genotype genotype) {
+        return new Animal(map, new Vector2d(0, 0), MapDirection.NORTH, genotype, 5, 5, () -> 0);
     }
 }
